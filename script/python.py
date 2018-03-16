8cb6ee4935fd6b2e09981af812e6de5f64707645
Akhil Raj <akhilrajmailbox@gmail.com>
2018-03-16 17:05:41 +0530

    Update python.py

--- script/python.py
+++ script/python.py
@@ -1,82 +1,115 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import ipaddress
 import sys
 import os
+
 
 if sys.version_info[0] < 3:
  print ('Must be using Python 3')
 else:
 
 
-## Connection option
- while True:
-    print ('')
-    print (' R for Remote Forwarding  ie : forward a port from the local machine to the server machine')
-    print (' L for Local Forwarding  ie : forward a port from the server machine ot the local machine')
-    print ('')
-    CON_OPTION = input('Choose R or L : ')
-    if CON_OPTION == "R" or CON_OPTION == "L":
-        if CON_OPTION == "R":
-           ## Remote Server User
-           USERNAME = input('Username of Remote Server for ssh Connection : ')
-        else:
-           ## Local System User
-           USERNAME = input('Username of Local Server for ssh Connection : ')
+## static Environment Variables
+ REMOTE_IP = '192.168.0.100'
+ REMOTE_PORT = '0'
+ DIRECTORY = '/savedir/'
+ USERNAME = 'remote_username'
+
+
+## Provider-Local port assigning
+ def provider_portassign():
+   print ('')
+   global LOCAL_PORT
+   LOCAL_PORT = input('Which Port is being Provided ? : ')
+   while True:
+      try:
+        int(LOCAL_PORT)
+      except ValueError:
+        print ('')
+        print ('Invalid Port number.......!')
+        LOCAL_PORT = input('Which Port is being Provided ? : ')
+        continue
+      else:
         break
 
 
-## Local port
- LOCAL_PORT = input('Local Port for Expose to Outside : ')
- while True:
-    try:
-      int(LOCAL_PORT)
-    except ValueError:
-      print ('Invalid Port number.......!')
-      LOCAL_PORT = input('Local Port for Expose to Outside : ')
-      continue
-    else:
-      break
+## Provider-Saving Dynamic ports of remote server and Private Key
+ def provider_portsave():
+   provider_portassign()
+   if not os.path.exists(DIRECTORY):
+        os.makedirs(DIRECTORY)
 
 
-## Remote port
- REMOTE_PORT = input('Remote Port for Pointing to Local Port : ')
- while True:
-    try:
-      int(REMOTE_PORT)
-    except ValueError:
-      print ('Invalid Port number.......!')
-      REMOTE_PORT = input('Remote Port for Pointing to Local Port : ')
-      continue
-    else:
-      break
-
-
-## Remote IP Address
- REMOTE_IP = input('Remote Server IP Address : ')
- while True:
+## Consumer-Remote port consuming
+ def consumer_portconsume():
+   print ('')
+   global REMOTE_PORT
+   REMOTE_PORT = input('Which Port is being Consumed ? : ')
+   while True:
       try:
-         ip = ipaddress.ip_address(REMOTE_IP)
+        int(REMOTE_PORT)
       except ValueError:
-         print ('Invalid IP Address.......!')
-         REMOTE_IP = input('Remote Server IP Address : ')
-         continue
+        print ('Invalid Port number.......!')
+        REMOTE_PORT = input('Which Port is being Consumed ? : ')
+        continue
       else:
-         break
+        break
 
 
- data = {"CON_OPTION": CON_OPTION,
-        "REMOTE_IP": REMOTE_IP,
+## Consumer-Local port assigning
+ def consumer_portassign():
+   consumer_portconsume()
+
+   print ('')
+   global LOCAL_PORT
+   LOCAL_PORT = input('Which Port is being Exposed ? : ')
+   while True:
+      try:
+        int(LOCAL_PORT)
+      except ValueError:
+        print ('Invalid Port number.......!')
+        LOCAL_PORT = input('Which Port is being Exposed ? : ')
+        continue
+      else:
+        break
+
+
+
+## Connection option
+ while True:
+    print ('')
+    print (' Am I in provider(P) node or Consumer(C) node ?')
+    print (' Choose P or C')
+    print ('')
+    CON_OPTION = input('Choose P or C : ')
+    if CON_OPTION == "P" or CON_OPTION == "C":
+        if CON_OPTION == "P":
+           provider_portsave()
+
+        elif CON_OPTION == "C":
+           print ('')
+           ## Local System User
+           USERNAME = input('Username of Local Server for ssh Connection ?: ')
+           consumer_portassign()
+
+        break
+
+
+ data = {"REMOTE_IP": REMOTE_IP,
         "LOCAL_PORT": LOCAL_PORT,
         "REMOTE_PORT": REMOTE_PORT,
+        "DIRECTORY": DIRECTORY,
         "USERNAME": USERNAME}
 
 
 ## Connection Establishment
- if CON_OPTION == "R":
+ if CON_OPTION == "P":
    ## Remote Server User
-   cmd = "ssh -fN -{CON_OPTION} {REMOTE_PORT}:localhost:{LOCAL_PORT} {USERNAME}@{REMOTE_IP}"
+   print ('')
+   print ('You can find the Server port details at : ' + DIRECTORY)
+   cmd = "ssh -fN -R {REMOTE_PORT}:localhost:{LOCAL_PORT} {USERNAME}@{REMOTE_IP} 2> {DIRECTORY}/{LOCAL_PORT}.txt"
    os.system(cmd.format(**data))
- else:
+ elif CON_OPTION == "C":
    ## Local System User
-   cmd = "ssh -fN -{CON_OPTION} \*:{LOCAL_PORT}:{REMOTE_IP}:{REMOTE_PORT} {USERNAME}@localhost"
+   cmd = "ssh -fN -L \*:{LOCAL_PORT}:{REMOTE_IP}:{REMOTE_PORT} {USERNAME}@localhost"
    os.system(cmd.format(**data))
